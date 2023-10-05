87a246e2df506a0cb99c20efa29a0cbdf9a44869
EmnAsPW <emonsourov@gmail.com>
2023-10-05 10:03:16 +0600

    mvc architecture

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const app = express();
-const PORT =3000;
+const express = require("express");
 
-app.get("/users", (req,res)=>{
-    res.send("Hello users");
+const userRouter = require("./routes/users.route");
+const productRouter = require("./routes/products.route");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(userRouter);
+app.use(productRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    message: "resource not found",
+  });
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is running http://localhost:${PORT}`);
 });
---
+++ models/users.model.js
@@ -0,0 +1,12 @@
+const users = [
+    {
+      name: "sourov",
+      age: 25,
+    },
+    {
+      name: "sam",
+      age: 47,
+    },
+  ];
+  
+  module.exports = users;
